9fe6f0b5eac3e3d4cb3641a948c5468c93d7037a
Matheus Alves <matheusalvessilvabr@gmail.com>
2023-07-10 16:34:55 -0300

    test createService

--- src/test/java/com/MyFirstProject/myprojectweb/service/CreateService.java
+++ src/test/java/com/MyFirstProject/myprojectweb/service/CreateService.java
@@ -5,6 +5,13 @@ import org.junit.Test;
 public class CreateService {
     @Test
     public void should_able_to_create_new_service() {
-    }
 
+        User user = new User();
+        user.setWorlkload(100);
+
+        CreateUserService createUserService = new CreateUserService();
+        User createUser = CreateUserService.execute(User);
+
+        assertNotNull(createUser.getid());
+    }
 }
