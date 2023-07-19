de67f7719e569f4ddc2d14ce1cfebf6b1de92031
Matheus Alves <matheusalvessilvabr@gmail.com>
2023-07-19 11:45:34 -0300

    remove service test and add get test

--- src/test/java/com/MyFirstProject/myprojectweb/controller/UserControllerTest.java
+++ src/test/java/com/MyFirstProject/myprojectweb/controller/UserControllerTest.java
@@ -2,21 +2,34 @@ package com.MyFirstProject.myprojectweb.controller;
 
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(controllers = UserController.class)
 @ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 public class UserControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
 
     @Test
-    void getAllUsersTest() throws Exception{
-        mockMvc.perform(get())
+    public void getAllUsersTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/user")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.employees").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.employees[*].employeeId").isNotEmpty());
     }
 }
