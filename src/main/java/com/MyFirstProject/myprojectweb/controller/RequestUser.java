29e53056ed078d0ffda0463fd53224ae6a4062f0
Matheus Alves <matheusalvessilvabr@gmail.com>
2023-08-06 09:28:58 -0300

    remove jakarta from RequestUser

--- src/main/java/com/MyFirstProject/myprojectweb/controller/RequestUser.java
+++ src/main/java/com/MyFirstProject/myprojectweb/controller/RequestUser.java
@@ -1,8 +1,5 @@
 package com.MyFirstProject.myprojectweb.controller;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 public record RequestUser(
         Long id,
         String username,
