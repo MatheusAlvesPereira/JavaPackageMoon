db4321b2c527b5ac0fdce0f887f7c3774f66f700
Matheus Alves <matheusalvessilvabr@gmail.com>
2023-11-10 09:05:07 -0300

    new hello world msg

--- src/main/java/com/MyFirstProject/myprojectweb/MyprojectwebApplication.java
+++ src/main/java/com/MyFirstProject/myprojectweb/MyprojectwebApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyprojectwebApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("--Hello JavaPackageMoon--");
 		SpringApplication.run(MyprojectwebApplication.class, args);
 	}
 }
